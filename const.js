af6ba6e3a83cc98bf807bb0d05fe9fa8fad0044f
Lam Huynh <tlamhuynh@gmail.com>
2017-02-06 14:08:44 +0700

    update config

--- const.js
+++ const.js
@@ -2,7 +2,7 @@
 
 // Wit.ai parameters
 //const WIT_TOKEN = process.env.WIT_TOKEN;
-const WIT_TOKEN = "ZH62LCXDJ4PQDSN6IMWJ77BA6GI262TP";
+const WIT_TOKEN = "KMY6WYXP3XYHQEUY5VQE2G3EZEU3IAEQ";
 if (!WIT_TOKEN) {
   throw new Error('missing WIT_TOKEN');
 }
@@ -13,7 +13,7 @@ const FB_PAGE_TOKEN = "EAAEAk3NaaAYBAFlKf7Dd2ehaZAoo3bnuYLfkKzZAAEqZBGwgYpAfSwiL
 
 //const FB_APP_SECRECT = process.env.FB_APP_SECRECT;
 
-const FB_APP_SECRECT = "5a0c8ecb38855d9aba177211203764db";
+const FB_APP_SECRECT = "7091c96672b5baee5a13991ef64f8976";
 
 var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 
