98495a1cf441b6a800f6334eb602bc9c8262c419
nguyentrucanhtuan <nguyentrucanhtuan@gmail.com>
2017-04-13 09:49:39 +0700

    Update const.js

--- const.js
+++ const.js
@@ -9,16 +9,16 @@ if (!WIT_TOKEN) {
 
 // Messenger API parameters
 //const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
-const FB_PAGE_TOKEN = "EAAEAk3NaaAYBAFlKf7Dd2ehaZAoo3bnuYLfkKzZAAEqZBGwgYpAfSwiL7SESYuxfb1cZBIYyZCuLJmLIrwt3YdbU3OXQyC7ZCSKpZCdreqEsHd3zesdCstdnKfh5PKSTZBf3RgUCKlf0Pxl8z59Lhdyc3am0iRtLmPuDv7VSSZBuMrQZDZD";
+const FB_PAGE_TOKEN = "EAAHwsu50wLoBAMay8ny7ZCmN7grBv4SXN2nmWnskKUXCTdcBMXWjnybN7RALineouL4Ui9ZClJGDgUQoprhqXReu9cCUuNyM0v0JuPEZBzyQqcgVrPYOSquJ9ZAs6LtKF5Y9QL27MxoI8BZAGw1B6fZCE11HDiFhK1vL6VIYqHOwZDZD";
 
 //const FB_APP_SECRECT = process.env.FB_APP_SECRECT;
 
-const FB_APP_SECRECT = "7091c96672b5baee5a13991ef64f8976";
+const FB_APP_SECRECT = "5a0c8ecb38855d9aba177211203764db";
 
 var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 
 if (!FB_VERIFY_TOKEN) {
-  FB_VERIFY_TOKEN = "tlamhuynhsmartbot";
+  FB_VERIFY_TOKEN = "tntfbbotauto";
 }
 
 
